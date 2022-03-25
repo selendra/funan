a2320876a1b4e18ca99df41bf3fe15bd680f5eef
san-vuthy <san.vuthy08@gmail.com>
2022-03-25 09:04:05 +0700

    update icons

--- src/components/ButtonConnect.js
+++ src/components/ButtonConnect.js
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Menu, Button, Dropdown} from "antd";
+import { Menu, Button, Dropdown } from "antd";
 import { AccountContext } from "../context/AccountContext";
 import { shortenAddress } from "../utils";
 
 export default function ButtonConnect() {
   const { account, disconnect } = useContext(AccountContext);
   const menu = (
-    <Menu className='btn-connect-menu'>
-      <Menu.Item key="0" className='btn-connect-menuItem' onClick={disconnect}>
+    <Menu className="btn-connect-menu">
+      <Menu.Item key="0" className="btn-connect-menuItem" onClick={disconnect}>
         <span>Disconnect</span>
       </Menu.Item>
     </Menu>
@@ -16,17 +16,22 @@ export default function ButtonConnect() {
 
   return (
     <div>
-      { account ? 
-        <Dropdown overlay={menu} trigger={['click']}>
-          <Button type='ghost' className='btn__connect'>
+      {account ? (
+        <Dropdown overlay={menu} trigger={["click"]}>
+          <Button type="ghost" className="btn__connect">
+            <img
+              src="/icons/bulk/wallet-3.svg"
+              alt="money-recive.svg"
+              height="30px"
+            />
             {shortenAddress(account)}
           </Button>
         </Dropdown>
-        :
-        <Button className='btn__connect'>
-          <Link to='/connect'>Connect</Link>
+      ) : (
+        <Button className="btn__connect">
+          <Link to="/connect">Connect</Link>
         </Button>
-      }
+      )}
     </div>
-  )
+  );
 }
